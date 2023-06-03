edf1de577d4bf9371d148d9fb87961439b82633e
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-03 11:15:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const errorMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found');
+const connectDB = require('./db/connect');
+
+app.use(errorMiddleware)
+app.use(express.json());
+app.use(notFoundMiddleware)
+
+app.use('/api/v1/products', require('./routes/products'))
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        connectDB(process.env.MONGO_URI);
+        console.log('Database connected');
+        app.listen(port, console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
