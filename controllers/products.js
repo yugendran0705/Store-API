235f2f9e0f8537273d3a5ecab222d963009dd849
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-03 12:54:16 +0530

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,16 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({ price: { $gt: 30 } })
-        .sort('name')
-        .select('name price');
-
+    const products = await Product.find({ featured: true });
     res.status(200).json({ products, nbHits: products.length });
+}
+
+const getAllProducts = async (req, res) => {
+    const products = await Product.find(req.query);
+    res.status(200).json({ products, nbHits: products.length });
+}
+
+module.exports = {
+    getAllProducts,
+    getAllProductsStatic,
 }
