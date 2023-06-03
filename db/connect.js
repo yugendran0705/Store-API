f1a7e497bb6e98caed94f507a5e287d48fc3240c
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-03 12:54:02 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose')
 
 const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
+  mongoose
+    .connect(url,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      })
+    .then(() => {
+      console.log("MongoDB connected")
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
 
 module.exports = connectDB
