0a456ddaca08f5b7891990879972da1cba2ae21d
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-03 12:53:47 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -2,15 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {
     getAllProducts,
-    createProduct,
-    getProduct,
-    updateProduct,
-    deleteProduct,
-    uploadProductImage
-} = require('../controllers/products');
+    getAllProductsStatic,
+} = require('../controllers/products')
 
-router.route('/').get(getAllProducts).post(createProduct);
-router.route('/:id').get(getProduct).patch(updateProduct).delete(deleteProduct);
-router.route('/:id/upload').post(uploadProductImage);
+router.route('/').get(getAllProducts)
+router.route('/static').get(getAllProductsStatic)
 
 module.exports = router;
